5132a195e1fa785b9156ee297de21ed7696e58ce
boolgyeowoo <boolgyeowoo@gmail.com>
2020-09-22 12:35:53 +0900

    css edit
    edited index.css to change elements position and flex layout

--- bgimg.js
+++ bgimg.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    if(window.innerWidth<=767) {
+    if((window.innerWidth / window.innerHeight) <=0.875) {
         var background_img = "img/index/home_s_"; /*small(mobile) size window*/
         var number = Math.floor(Math.random() * 5) + 1;
         var container = document.getElementById("bgImage");
