ffe77ba2c8a239c346092f071437cfdbec824ac2
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-23 12:35:27 +0900

    refactoring
    delete nav_mobl tags

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,9 +1,28 @@
 jQuery(document).ready(function ($) {
-    $(".nav_mobl").hide();
-    $(".menu_icon").click(function () {
-        $(".nav_mobl").slideToggle(300);
-    });
-    $(window).resize(function () {
-        $(".nav_mobl").hide();
-    });
+//    if (window.innerWidth < 800) {
+//        $(".nav_main").hide();
+//        $(".menu_icon").click(function () {
+//            $(".nav_main").slideToggle(300);
+//        });
+//        $(window).resize(function () {
+//            $(".nav_main").hide();
+//        });
+//    } else {
+//        $(window).resize(function () {
+//            $(".nav_main").show();
+//        });
+//    }
+    if ($(window).resize() === true) {
+        if (window.innerWidth < 800) {
+            $(".nav_mobl").hide();
+            $(".menu_icon").click(function () {
+                $(".nav_mobl").slideToggle(300);
+            });
+            $(window).resize(function () {
+                $(".nav_mobl").hide();
+            });
+        } else {
+            $(".nav_main").show();
+        }
+    }
 });
