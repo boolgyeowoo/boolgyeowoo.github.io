5d3e484a695f864fd6fff09826171dc0f98a6f4a
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-23 13:32:53 +0900

    update mobl_nav.js
    todo: bug fix

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,28 +1,24 @@
 jQuery(document).ready(function ($) {
-//    if (window.innerWidth < 800) {
-//        $(".nav_main").hide();
-//        $(".menu_icon").click(function () {
-//            $(".nav_main").slideToggle(300);
-//        });
-//        $(window).resize(function () {
-//            $(".nav_main").hide();
-//        });
-//    } else {
-//        $(window).resize(function () {
-//            $(".nav_main").show();
-//        });
-//    }
-    if ($(window).resize() === true) {
-        if (window.innerWidth < 800) {
-            $(".nav_mobl").hide();
-            $(".menu_icon").click(function () {
-                $(".nav_mobl").slideToggle(300);
-            });
-            $(window).resize(function () {
-                $(".nav_mobl").hide();
-            });
-        } else {
-            $(".nav_main").show();
-        }
-    }
+    if (window.innerWidth >= 800) {
+        $('.nav_main').show();
+        $(window).resize(function () {
+           if (window.innerWidth >= 800) {
+                $('.nav_main').show();
+            } else {
+                $(".nav_main").hide();
+            } 
+        });
+    } else {
+        $(".nav_main").hide();
+        $(".menu_icon").click(function () {
+            $(".nav_main").slideToggle(300);
+        });
+        $(window).resize(function () {
+            if (window.innerWidth >= 800) {
+                $('.nav_main').show();
+            } else {
+                $(".nav_main").hide();
+            }
+        });
+    };
 });
