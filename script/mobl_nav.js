3f6c04710706c03d742bf3bada5814a3156e4374
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-23 14:03:00 +0900

    update mobl_nav.js
    using js to control display attribute of .menu_icons

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,14 +1,21 @@
 jQuery(document).ready(function ($) {
     if (window.innerWidth >= 800) {
         $('.nav_main').show();
+        $('.menu_icon').hide();
         $(window).resize(function () {
-           if (window.innerWidth >= 800) {
+            if (window.innerWidth >= 800) {
                 $('.nav_main').show();
+                $('.menu_icon').hide();
             } else {
+                $(".menu_icon").show();
                 $(".nav_main").hide();
-            } 
+                $(".menu_icon").click(function () {
+                    $(".nav_main").slideToggle(300);
+                });
+            }
         });
     } else {
+        $(".menu_icon").show();
         $(".nav_main").hide();
         $(".menu_icon").click(function () {
             $(".nav_main").slideToggle(300);
@@ -16,8 +23,13 @@ jQuery(document).ready(function ($) {
         $(window).resize(function () {
             if (window.innerWidth >= 800) {
                 $('.nav_main').show();
+                $(".menu_icon").hide();
             } else {
+                $('.menu_icon').show();
                 $(".nav_main").hide();
+                $(".menu_icon").click(function () {
+                    $(".nav_main").slideToggle(300);
+                });
             }
         });
     };
