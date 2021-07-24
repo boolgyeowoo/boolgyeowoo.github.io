78dd7729307ed421f1020c4d5aed94738b58b650
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-24 16:12:42 +0900

    testing ajax nav

--- fetchpage.js
+++ fetchpage.js
@@ -1,3 +1,20 @@
+fetch('navlist').then(function(response){
+    response.text().then(function(text){
+        var linkNames = text.split(',');
+        var i = 0;
+        var mainNav = '';
+        while(i<linkNames.length){
+            var linkName = linkNames[i];
+            linkName = linkName.trim();
+            var navLi = '<li><a class="nav_link" href="#!'+linkName+'" onclick="fetchPage(\''+linkName+'\')">'+linkName+'</a></li>';
+            mainNav = mainNav + navLi;
+            i = i + 1;
+        }
+        document.querySelector('#nav_main').innerHTML = mainNav;
+    });
+});
+
+
 function fetchPage(pgName) {
     fetch(pgName).then(function (response) {
         response.text().then(function (text) {
