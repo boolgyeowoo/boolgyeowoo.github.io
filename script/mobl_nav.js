f5ad47043c3e172a82e7c2bb5fa291072ec45738
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-24 10:34:07 +0900

    changed to control #menu_icon in css

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,30 +1,23 @@
 jQuery(document).ready(function ($) {
 
-    var btn = '#menu_icon';
     var nav = '#nav_main';
 
     if (window.innerWidth >= 800) {
         $(nav).show();
-        $(btn).hide();
         $(window).resize(function () {
             if (window.innerWidth >= 800) {
             $(nav).show();
-            $(btn).hide();
         } else {
-            $(btn).show();
             $(nav).hide();
         }
         });
     } else {
-        $(btn).show();
         $(nav).hide();
-//        $(btn).click(function () {$('#nav_main').slideToggle(300);});
+//        $('#menu_icon').click(function () {$('#nav_main').slideToggle(300);});
         $(window).resize(function () {
             if (window.innerWidth >= 800) {
             $(nav).show();
-            $(btn).hide();
         } else {
-            $(btn).show();
             $(nav).hide();
         }
         });
