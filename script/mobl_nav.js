43c8de66badb722db4b9e8b1e01982b771866260
boolgyeowoo <boolgyeowoo@gmail.com>
2021-07-24 09:39:40 +0900

    fix nav bug
    -update mobl_nav.js
    -add js code in html file

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -3,46 +3,30 @@ jQuery(document).ready(function ($) {
     var btn = '#menu_icon';
     var nav = '#nav_main';
 
-    function btnClick() {
-        $(nav).slideToggle(300);
-    }
-
-    //    function showHideIf() {
-    //        if (window.innerWidth >= 800) {
-    //            $(nav).show();
-    //            $(btn).hide();
-    //        } else {
-    //            $(btn).show();
-    //            $(nav).hide();
-    //        }
-    //    }
-
     if (window.innerWidth >= 800) {
         $(nav).show();
         $(btn).hide();
         $(window).resize(function () {
             if (window.innerWidth >= 800) {
-                $(nav).show();
-                $(btn).hide();
-            } else {
-                $(btn).show();
-                $(nav).hide();
-            }
+            $(nav).show();
+            $(btn).hide();
+        } else {
+            $(btn).show();
+            $(nav).hide();
+        }
         });
     } else {
         $(btn).show();
         $(nav).hide();
-        $(btn).click(function () {
-            $(nav).slideToggle(300);
-        });
+//        $(btn).click(function () {$('#nav_main').slideToggle(300);});
         $(window).resize(function () {
             if (window.innerWidth >= 800) {
-                $(nav).show();
-                $(btn).hide();
-            } else {
-                $(btn).show();
-                $(nav).hide();
-            }
+            $(nav).show();
+            $(btn).hide();
+        } else {
+            $(btn).show();
+            $(nav).hide();
+        }
         });
     }
 });
