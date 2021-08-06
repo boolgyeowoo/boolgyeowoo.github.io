0d415315ae6d5ea13ae87ab06b44383334431573
boolgyeowoo <boolgyeowoo@gmail.com>
2021-08-06 14:20:14 +0900

    testing other source
    src = https://jsfiddle.net/getbutterfly/bs79njcs/

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,46 +1,30 @@
 window.onload = function () {
 
     var nav = document.getElementById('nav_main');
-    var icon = document.getElementById('menu_icon');
+//    var icon = document.getElementById('menu_icon');
     var show = function (elem) {
         elem.style.display = 'flex';
     };
     var hide = function (elem) {
         elem.style.display = 'none';
     };
+    
     function rsWidth(elem) {
         if (window.innerWidth >= 800) {
             show(elem);
         } else {
             hide(elem);
         }
-    };
+    }
     
-    let container = nav;
-    icon.onclick.addEventListener('click' function(event) {
-        event.preventDefault();
-
-        if (!container.classList.contains('active')) {
-            container.classList.add('active');
-            container.style.height = 'auto';
-
-            let height = container.clientHeight + "px";
-
-            container.style.height = '0px';
-
-            setTimeout(function () {
-                container.style.height = height;
-            }, 0);
-        } else {
-            container.style.height = '0px';
-
-            container.addEventListener('transitionend', function () {
-                container.classList.remove('active');
-            }, {
-                once: true
-            });
-        }
-    });
+//    var clickToggle = function(click) {
+//        if (nav.style.display == 'none') {
+//            nav.style.height = 'auto';
+//            nav.style.overflow = 'visible';
+//        } else {
+//            nav.style.
+//        }
+//    };
     
     if (window.innerWidth >= 800) {
         show(nav);
