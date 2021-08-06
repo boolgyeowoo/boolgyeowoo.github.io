5b8803d0d82b8812f83ffe67f230d62e9d02f25b
boolgyeowoo <boolgyeowoo@gmail.com>
2021-08-06 12:25:31 +0900

    toggle test
    src = https://getbutterfly.com/how-to-implement-jquery-slidetoggle-in-vanilla-javascript/

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -16,14 +16,31 @@ window.onload = function () {
         }
     };
     
-    icon.onclick = function clickToggle(toggle) {
-        if (toggle.style.display == 'none') {
-            toggle.style.height = 0;
-            
+    let container = nav;
+    icon.onclick.addEventListener('click' function(event) {
+        event.preventDefault();
+
+        if (!container.classList.contains('active')) {
+            container.classList.add('active');
+            container.style.height = 'auto';
+
+            let height = container.clientHeight + "px";
+
+            container.style.height = '0px';
+
+            setTimeout(function () {
+                container.style.height = height;
+            }, 0);
         } else {
-            
+            container.style.height = '0px';
+
+            container.addEventListener('transitionend', function () {
+                container.classList.remove('active');
+            }, {
+                once: true
+            });
         }
-    };
+    });
     
     if (window.innerWidth >= 800) {
         show(nav);
