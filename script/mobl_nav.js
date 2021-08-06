6ee941809b3314a42256d84819e721ee4240f030
boolgyeowoo <boolgyeowoo@gmail.com>
2021-08-06 11:19:04 +0900

    Update mobl_nav.js
    moblie nav show hide function without jquery. (vanila javascript)

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,3 +1,4 @@
+/*
 jQuery(document).ready(function ($) {
 
     var nav = '#nav_main';
@@ -23,3 +24,43 @@ jQuery(document).ready(function ($) {
         });
     }
 });
+*/
+
+
+window.onload = function () {
+
+    var nav = document.getElementById('nav_main');
+    var show = function (elem) {
+        elem.style.display = 'flex';
+    };
+    var hide = function (elem) {
+        elem.style.display = 'none';
+    };
+    var rsWidth = function () {
+        if (window.innerWidth >= 800) {
+            show(nav);
+        } else {
+            hide(nav);
+        }
+    };
+
+    if (window.innerWidth >= 800) {
+        show(nav);
+        window.onresize = function () {
+            if (window.innerWidth >= 800) {
+                show(nav);
+            } else {
+                hide(nav);
+            }
+        };
+    } else {
+        hide(nav);
+        window.onresize = function () {
+            if (window.innerWidth >= 800) {
+                show(nav);
+            } else {
+                hide(nav);
+            }
+        };
+    }
+    };
