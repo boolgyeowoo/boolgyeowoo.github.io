98141b40c040d3ae08b499cc9fba62d832068425
boolgyeowoo <boolgyeowoo@gmail.com>
2021-08-06 11:23:23 +0900

    Update mobl_nav.js
    clear unused codes

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,32 +1,3 @@
-/*
-jQuery(document).ready(function ($) {
-
-    var nav = '#nav_main';
-
-    if (window.innerWidth >= 800) {
-        $(nav).show();
-        $(window).resize(function () {
-            if (window.innerWidth >= 800) {
-            $(nav).show();
-        } else {
-            $(nav).hide();
-        }
-        });
-    } else {
-        $(nav).hide();
-//        $('#menu_icon').click(function () {$('#nav_main').slideToggle(300);});
-        $(window).resize(function () {
-            if (window.innerWidth >= 800) {
-            $(nav).show();
-        } else {
-            $(nav).hide();
-        }
-        });
-    }
-});
-*/
-
-
 window.onload = function () {
 
     var nav = document.getElementById('nav_main');
@@ -36,13 +7,14 @@ window.onload = function () {
     var hide = function (elem) {
         elem.style.display = 'none';
     };
-    var rsWidth = function () {
+/*    var rsWidth = function () {
         if (window.innerWidth >= 800) {
             show(nav);
         } else {
             hide(nav);
         }
     };
+*/
 
     if (window.innerWidth >= 800) {
         show(nav);
@@ -63,4 +35,4 @@ window.onload = function () {
             }
         };
     }
-    };
+};
