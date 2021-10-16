89d905a7a0e201b2cd8e65f04502e158f52851ca
boolgyeowoo <71315109+boolgyeowoo@users.noreply.github.com>
2021-10-16 15:19:24 +0900

    Update mobl_nav.js

--- script/mobl_nav.js
+++ script/mobl_nav.js
@@ -1,6 +1,6 @@
 var moblNav = function () {
 
-    var nav = document.getElementByClassName('nav_main');
+    var nav = document.getElementById('nav_main');
     var icon = document.getElementById('menu_icon');
     var show = function (elem) {
         elem.style.display = 'flex';
@@ -28,12 +28,12 @@ var moblNav = function () {
         };
     };
     icon.addEventListener('click', function (event) {
-        if (nav.className != 'nav_main open') {
-//            show(nav);
-            nav.className = 'nav_main open';
+        if (nav.className != 'open') {
+            show(nav);
+            nav.className = 'open';
         } else {
-//            hide(nav);
-            nav.className = 'nav_main';
+            hide(nav);
+            nav.className = '';
         };
     });
 }
